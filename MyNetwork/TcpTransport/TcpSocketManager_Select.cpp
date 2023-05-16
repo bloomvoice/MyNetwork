2fd717d2a82b966a45d768aa104c2e06dc10e749
bloomvoice <861946233@qq.com>
2023-05-16 23:29:53 +0800

    修改select函数文件

--- MyNetwork/TcpTransport/TcpSocketManager_Select.cpp
+++ MyNetwork/TcpTransport/TcpSocketManager_Select.cpp
@@ -24,8 +24,50 @@ namespace AzNetworking
 		AddSocketHelper(socketFd);
 
 		return true;
+	}
+
+	bool TcpSocketManager::ClearSocket(SocketFd socketFd)
+	{
+		FD_CLR(static_cast<int32_t>(socketFd), &m_sourceFdSet);
+		ClearSocketHelper(socketFd);
+		return true;
+	}
+
+	void TcpSocketManager::ProcessEvents(AzNetworking::TimeMS maxBlockMS, const SocketEventCallBack& readCallBack, const SocketEventCallBack& writeCallBack)
+	{
+		if (m_maxFd <= SocketFd(0) || m_socketFds.empty())
+		{
+			return;
+		}
+
+		m_readerFdSet = m_sourceFdSet;
+		m_writerFdSet = m_sourceFdSet;
+
+		struct timeval tv = { 0, static_cast<int32_t>(maxBlockMS) * 1000 };
+		const int32_t selectResult = ::select(static_cast<int32_t>(m_maxFd) + 1, &m_readerFdSet, &m_writerFdSet, nullptr, &tv);
+		if (selectResult < 0)
+		{
+			const int32_t error = GetLastNetworkError();
+			AZLOG_ERROR("select returned an error(%d:%s)", error, GetNetworkErrorDesc(error));
+		}
+
+
+		for (auto socketFd : m_socketFds)
+		{
+			if (FD_ISSET(static_cast<int32_t>(socketFd), &m_readerFdSet))
+			{
+				readCallBack(socketFd);
+			}
+
+			if (FD_ISSET(static_cast<int32_t>(socketFd), &m_writerFdSet))
+			{
+				writeCallBack(socketFd);
+			}
+		}
 
 	}
+
+
 }
 
 #endif // AZ_TRAIT_USE_SOCKET_SERVER_SELECT
